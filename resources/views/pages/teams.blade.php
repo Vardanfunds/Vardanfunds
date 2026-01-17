3bee20956698ebbbeee11039c80f2609221f912b
Sumeet_Gond <sumitv9009@gmail.com>
2026-01-17 15:40:15 +0530

    feat: Add team listing page, new global stylesheet, and mobile responsiveness task list.

--- resources/views/pages/teams.blade.php
+++ resources/views/pages/teams.blade.php
@@ -16,19 +16,25 @@
 <!-- TEAM GRID -->
 <div class="container py-5 text-center">
 
-    <div class="row gy-5">
+    <div class="row g-4"> <!-- Added gap -->
         @isset($teamsModel)
-            @foreach ($teamsModel as $key=> $item) 
-                <!-- Row 1 -->
-                <div class="col-6 col-md-4">
-                    <img src="{{ asset($item->img) }}" style="width:210px; height:210px;" alt="Team Image">
-                    <div class="text-danger mt-2" style="font-size:13px; font-weight:600;"><a href="{{ ('team/view').'/'.$item['id'] }}" style="color:black">{{ $item['name'] }}</a></div>
+            @foreach ($teamsModel as $key => $item) 
+                <div class="col-6 col-md-4 col-lg-3"> <!-- Added col-lg-3 for better desktop view -->
+                    <div class="team-item">
+                        <div class="team-img-wrapper">
+                            <a href="{{ ('team/view').'/'.$item['id'] }}">
+                                <img src="{{ asset($item->img) }}" class="img-fluid w-100 team-image" alt="{{ $item['name'] }}">
+                            </a>
+                        </div>
+                        <div class="mt-3 text-center">
+                            <h5 class="team-name mb-0">
+                                <a href="{{ ('team/view').'/'.$item['id'] }}" class="text-dark text-decoration-none">{{ $item['name'] }}</a>
+                            </h5>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         @endisset
-       
-       
-      
     </div>
 </div>
 
